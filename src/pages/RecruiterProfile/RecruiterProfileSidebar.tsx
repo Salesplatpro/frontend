fe99242eb1329c85e858830a4a107883af78b71e
Timmydee <dahunsitimmy@gmail.com>
2024-07-12 10:22:55 +0100

    fixes

--- src/pages/RecruiterProfile/RecruiterProfileSidebar.tsx
+++ src/pages/RecruiterProfile/RecruiterProfileSidebar.tsx
@@ -4,12 +4,11 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 
 import logo from '../../assets/logo.png'
-import { useAuth } from '../../context/contextHook'
-import { SideBar } from "../../components";
+import { SideBar } from '../../components'
+import { useSelector } from 'react-redux'
+import { RootState } from '../../redux/store/store'
 
 const RecruiterProfileSidebar = () => {
-  const auth = useAuth()
-
   return (
     <div className="dashboard">
       <div className="sidebar-container">
@@ -17,18 +16,7 @@ const RecruiterProfileSidebar = () => {
       </div>
 
       <div className="dashboard-body">
-        {!auth?.isLoggedIn ? (
-          <div>
-            <h6>You need to login</h6>
-            <div className="action-btn nav-font">
-              <button className="login" type="button">
-                <a href="/login">login</a>
-              </button>
-            </div>
-          </div>
-        ) : (
-          <Outlet />
-        )}
+        <Outlet />
       </div>
     </div>
   )
