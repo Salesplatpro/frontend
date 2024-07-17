f81a40930bf694d1bff24432b128c72643f74e6e
Timmydee <dahunsitimmy@gmail.com>
2024-07-17 13:06:34 +0100

    feat: support and feedback item

--- src/components/sidebar/sidebarData.tsx
+++ src/components/sidebar/sidebarData.tsx
@@ -8,33 +8,46 @@ import { CgProfile } from 'react-icons/cg'
 const ICON_SIZE = 24
 const COLOR = '#4985DF'
 
-export const sidebarData = [
-  {
-    name: 'Dashboard',
-    icon: <RxDashboard size={ICON_SIZE} color={COLOR} />,
-    link: '/recruiterDashboard/postjob',
-  },
-  {
-    name: 'My Job Listing',
-    icon: <MdWorkOutline size={ICON_SIZE} color={COLOR} />,
-    link: '/recruiterDashboard/jobProfiles',
-  },
-  {
-    name: 'Notification',
-    icon: <BiMessageDetail size={ICON_SIZE} color={COLOR} />,
-    count: 12,
-  },
-  {
-    name: 'Chat',
-    icon: <BsChatDots size={ICON_SIZE} color={COLOR} />,
-    count: 10,
-  },
-  {
-    name: 'Profile',
-    icon: <CgProfile size={ICON_SIZE} color={COLOR} />,
-  },
+// export const sidebarData = [
+//   {
+//     name: 'Dashboard',
+//     icon: <RxDashboard size={ICON_SIZE} color={COLOR} />,
+//     link: '/recruiterDashboard/postjob',
+//   },
+//   {
+//     name: 'My Job Listing',
+//     icon: <MdWorkOutline size={ICON_SIZE} color={COLOR} />,
+//     link: '/recruiterDashboard/jobProfiles',
+//   },
+//   {
+//     name: 'Notification',
+//     icon: <BiMessageDetail size={ICON_SIZE} color={COLOR} />,
+//     count: 12,
+//   },
+//   {
+//     name: 'Chat',
+//     icon: <BsChatDots size={ICON_SIZE} color={COLOR} />,
+//     count: 10,
+//   },
+//   {
+//     name: 'Profile',
+//     icon: <CgProfile size={ICON_SIZE} color={COLOR} />,
+//   },
+//   {
+//     name: 'Support',
+//     icon: <MdOutlineContactSupport size={ICON_SIZE} color={COLOR} />,
+//   },
+// ]
+
+export const feedBack = [
   {
     name: 'Support',
     icon: <MdOutlineContactSupport size={ICON_SIZE} color={COLOR} />,
+    link: '/talentDashboard/TalentProfile',
+  },
+  {
+    name: 'Leave us feedBack',
+    icon: <CgProfile size={ICON_SIZE} color={COLOR} />,
+    link: '/talentDashboard/job',
   },
 ]
