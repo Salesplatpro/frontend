9a0e3bc48b90ac4f97ec40b050fa78ccbb319c1d
Timmydee <dahunsitimmy@gmail.com>
2024-07-17 13:08:36 +0100

    feat

--- src/redux/features/reducer.ts
+++ src/redux/features/reducer.ts
@@ -1,10 +1,12 @@
 import authSlice from './authSlice/authSlice'
 import { combineReducers } from '@reduxjs/toolkit'
 import { api } from '../api/apiSlice'
+import { talentApi } from '../api/talent'
 
 const rootReducer = combineReducers({
   auth: authSlice,
   [api.reducerPath]: api.reducer,
+  [talentApi.reducerPath]: talentApi.reducer,
 })
 
 export default rootReducer
--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,11 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import rootReducer from '../features/reducer'
 import { api } from '../api/apiSlice'
+import { talentApi } from '../api/talent'
 
 export const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(api.middleware),
+    getDefaultMiddleware().concat(api.middleware, talentApi.middleware),
   devTools: process.env.NODE_ENV !== 'production', // Enable Redux DevTools in development
 })
 
