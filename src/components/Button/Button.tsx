969057a32c8c224976771d0c566b743a3569606b
mosidrum <mosiokanga@gmail.com>
2024-07-27 23:32:06 +0100

    feat: clean up & validate login form

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,19 +1,28 @@
-import React, { ReactNode } from "react";
-import styles from './Button.module.scss';
+import React, { ReactNode } from 'react'
+
+import styles from './Button.module.scss'
 
 type ButtonProps = {
-  element?: ReactNode;
-  title: string;
-  variant?: 'primary' | 'secondary',
-  onClick?: () => void;
+  element?: ReactNode
+  title: string
+  variant?: 'primary' | 'secondary'
+  onClick?: () => void
 }
 
-export const Button = ({ title, element, variant = 'primary', onClick }: ButtonProps) => {
+export const Button = ({
+  title,
+  element,
+  variant = 'primary',
+  onClick,
+}: ButtonProps) => {
   const isSecondary = variant === 'secondary'
   return (
-    <button type="submit" className={isSecondary ? styles.secondary : styles.primary} onClick={onClick}>
+    <button
+      type="submit"
+      className={isSecondary ? styles.secondary : styles.primary}
+      onClick={onClick}>
       {element && <div className={styles.element}>{element}</div>}
       <div>{title}</div>
     </button>
-  );
+  )
 }
---
+++ src/pages/Auth/AuthValidationSchema.ts
@@ -0,0 +1,27 @@
+import * as Yup from 'yup'
+
+const emailValidation = () =>
+  Yup.string()
+    .test('valid-email', 'Invalid email format', (value) =>
+      value ? /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value) : false,
+    )
+    .required('Email is required')
+
+const passwordValidation = () =>
+  Yup.string()
+    .min(6, 'Password must be be at least 6 characters')
+    .required('Password is required')
+    .test('uppercase', 'Password must contain uppercase', (value) =>
+      /[A-Z]/.test(value),
+    )
+    .test('number', 'Password must have a number', (value) => /\d/.test(value))
+    .test(
+      'Special character',
+      'Password must contain special characters',
+      (value) => /[!#$%&*@^_]/.test(value),
+    )
+
+export const loginSchema = Yup.object().shape({
+  email: emailValidation(),
+  password: passwordValidation(),
+})
