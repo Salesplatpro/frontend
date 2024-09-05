40c68048f9065a97bd98c8d684b5af404b1d000c
mosidrum <mosiokanga@gmail.com>
2024-09-05 17:17:42 +0100

    feat: add document uploader component

---
+++ src/components/Loading/Uploader.tsx
@@ -0,0 +1,62 @@
+import Box from '@mui/material/Box'
+import LinearProgress, {
+  LinearProgressProps,
+} from '@mui/material/LinearProgress'
+import Typography from '@mui/material/Typography'
+import * as React from 'react'
+
+interface LinearProgressWithLabelProps extends LinearProgressProps {
+  value: number
+}
+
+const LinearProgressWithLabel = ({
+  value,
+  ...props
+}: LinearProgressWithLabelProps) => (
+  <Box sx={{ display: 'flex', alignItems: 'center' }}>
+    <Box sx={{ width: '100%', mr: 2 }}>
+      <LinearProgress variant="determinate" value={value} {...props} />
+    </Box>
+    <Box sx={{ minWidth: 35 }}>
+      <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+        {`${Math.round(value)}%`}
+      </Typography>
+    </Box>
+  </Box>
+)
+
+interface UploaderProps {
+  initialProgress?: number
+  interval?: number
+  increment?: number
+  maxProgress?: number
+}
+
+export const Uploader = ({
+  initialProgress = 1,
+  interval = 800,
+  increment = 1,
+  maxProgress = 100,
+}: UploaderProps) => {
+  const [progress, setProgress] = React.useState(initialProgress)
+
+  React.useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress((prevProgress) =>
+        prevProgress >= maxProgress
+          ? initialProgress
+          : prevProgress + increment,
+      )
+    }, interval)
+    return () => clearInterval(timer)
+  }, [initialProgress, increment, interval, maxProgress])
+
+  return (
+    <Box sx={{ width: '80%' }}>
+      <LinearProgressWithLabel
+        value={progress}
+        style={{ height: 8, borderRadius: 8 }}
+      />
+    </Box>
+  )
+}
