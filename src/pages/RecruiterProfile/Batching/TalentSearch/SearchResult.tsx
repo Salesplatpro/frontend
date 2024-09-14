dd3ea5713835e8e5f8cca2bf92ff013485c71c55
Timmydee <dahunsitimmy@gmail.com>
2024-09-15 00:02:06 +0100

    refractor: fetched talent data and used search params

--- src/pages/RecruiterProfile/Batching/TalentSearch/SearchResult.tsx
+++ src/pages/RecruiterProfile/Batching/TalentSearch/SearchResult.tsx
@@ -1,42 +1,97 @@
-import React from 'react'
-import { useLocation, Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { useSearchParams, Link } from 'react-router-dom'
+import { AnalyzedPercentage } from '../AnalyzedPercentage'
+import { Alert } from '@mui/material'
+import { useSearchTalentDbQuery } from '../../../../redux/api/recruiter'
+import talentdb from '../../../../assets/talentdb.webp'
+import Loading from '../../../../components/Loading/Loading'
 
 const SearchResult = () => {
-  const location = useLocation()
-  const talents = location.state?.talents || []
+  const [searchParams] = useSearchParams()
+  const [talents, setTalents] = useState([])
+
+  const initialSearchValues = {
+    role: searchParams.get('role') || '',
+    description: searchParams.get('description') || '',
+    location: {
+      country: {
+        name: searchParams.get('countryName') || '',
+        geoId: searchParams.get('geoId') || null,
+      },
+    },
+    experienceLevel: searchParams.get('experienceLevel') || '',
+  }
+
+  const { data, isLoading, error } = useSearchTalentDbQuery(
+    initialSearchValues,
+    {
+      skip: !initialSearchValues,
+    },
+  )
+
+  useEffect(() => {
+    if (data?.data?.talents) {
+      setTalents(data.data.talents)
+    }
+  }, [data])
+
+  if (isLoading) {
+    return <Loading />
+  }
+
+  if (error) {
+    let errorMessage = 'An error occurred while fetching the data'
+    return <Alert severity="error">{errorMessage}</Alert>
+  }
 
   return (
     <div>
-      <div className="space-y-2 mt-4">
+      <div className="space-y-2 mt-4 mb-10">
         <h1 className="font-raleway text-[#101828] text-[32px] font-bold leading-[37.57px]">
           Talent Search Result
         </h1>
         <p className="font-raleway font-normal text-[20px] leading-[23.48px] text-[#101828]">
-          find qualified talents by searching
+          Find qualified talents by searching
         </p>
       </div>
-      <div className="md:w-[50%] w-[80%] mx-auto mt-10">
-        {talents.map((talent: any) => (
-          <div
-            key={talent.id}
-            className="flex border w-full items-center p-4 rounded-lg">
-            <div className="flex-1">
-              <h2 className="text-[#0D0C22] font-semibold">
-                {talent.firstName} {talent.lastName}
-              </h2>
-              <h2 className="text-[#0D0C22] text-sm">
-                {talent.profile.experience}
-              </h2>
-              <h2 className="text-black">{talent.profile.role.name}</h2>
+      {talents.length === 0 ? (
+        <Alert severity="error">No Talents fit this description</Alert>
+      ) : (
+        <div className="md:w-[60%] w-[80%] mx-auto md:max-w[528px]">
+          {talents.map((talent: any) => (
+            <div
+              key={talent.id}
+              className="flex border border-[#E4E7EC] w-full items-start justify-center rounded-lg p-3">
+              <div className="flex-1 flex space-x-4">
+                <img src={talentdb} alt="" className="w-9 h-9" />
+                <div>
+                  <h2 className="text-[#0D0C22] font-semibold">
+                    {talent.firstName} {talent.lastName}
+                  </h2>
+                  {talent.profile.role.map((item: any) => (
+                    <h2
+                      key={item.id}
+                      className="text-[#0D0C22] text-sm font-medium">
+                      {item.name}
+                    </h2>
+                  ))}
+                  <h2 className="text-[#0D0C22] text-sm font-medium">
+                    {talent.profile.experience}
+                  </h2>
+                </div>
+              </div>
+              <div className="flex flex-col space-y-1">
+                <div className="flex justify-end">
+                  <AnalyzedPercentage targetValue={talent.similarity} />
+                </div>
+                <Link to="#" className="text-[#3C6FD4] text-sm">
+                  See analysis
+                </Link>
+              </div>
             </div>
-            <div>
-              <Link to="#" className="text-[#3C6FD4] text-sm">
-                See analysis
-              </Link>
-            </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
