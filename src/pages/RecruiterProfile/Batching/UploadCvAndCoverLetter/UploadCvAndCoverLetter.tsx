ae65d8866cac4b815e265dd4a55836d7668ce414
mosidrum <mosiokanga@gmail.com>
2024-09-18 14:43:09 +0100

    fix: change cv and cover letter request logic

--- src/pages/RecruiterProfile/Batching/UploadCvAndCoverLetter/UploadCvAndCoverLetter.tsx
+++ src/pages/RecruiterProfile/Batching/UploadCvAndCoverLetter/UploadCvAndCoverLetter.tsx
@@ -1,10 +1,16 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
 
-import { RecruiterButton } from '../../../../components'
+import { IsProcessing, RecruiterButton } from '../../../../components'
 import { DocumentUploaderCard2 } from '../../../../components/Cards'
 import { PageHeaderTitle } from '../../../../components/PageHeaderTitle'
-import { addCvCoverLetter } from '../../../../redux/features/filesSlice/fileSlice'
+import { useUploadCvAndCoverLetterMutation } from '../../../../redux/api/recruiter'
+import {
+  addCvCoverLetter,
+  removeCvCoverLetter,
+  saveCvCoverLetterResult,
+} from '../../../../redux/features/filesSlice/fileSlice'
 import { RootState } from '../../../../redux/store/store'
 import styles from './UploadCvAndCoverLetter.module.scss'
 
@@ -12,18 +18,29 @@ type CvAndCoverLetterType = {
   cv: File | null
   coverLetter: File | null
 }
+
 const defaultValues: CvAndCoverLetterType = {
   cv: null,
   coverLetter: null,
 }
 
 export const UploadCvAndCoverLetter = () => {
+  const params = useParams()
   const cv = useRef<HTMLInputElement>(null)
   const cover = useRef<HTMLInputElement>(null)
   const dispatch = useDispatch()
   const [selectedFiles, setSelectedFiles] =
     useState<CvAndCoverLetterType>(defaultValues)
+  const [showAddButton, setShowAddButton] = useState<boolean>(true)
+  const [isReevaluating, setIsReevaluating] = useState(false)
+  const [allFilesProcessed, setAllFilesProcessed] = useState(false)
+  const [moreFilesToProcess, setMoreFilesToProcess] = useState(false)
   const files = useSelector((state: RootState) => state.file.cvCoverLetter)
+  const result = useSelector(
+    (state: RootState) => state.file.cvCoverLetterResults,
+  )
+  const scoutJobId = params.id ?? ''
+  const [handleUploadCvAndCoverLetter] = useUploadCvAndCoverLetterMutation()
 
   useEffect(() => {
     if (cv.current) cv.current.value = ''
@@ -34,26 +51,89 @@ export const UploadCvAndCoverLetter = () => {
     event: React.ChangeEvent<HTMLInputElement>,
     type: 'cv' | 'coverLetter',
   ) => {
-    const file = event.target.files?.[0]
-    if (file) {
-      setSelectedFiles((prevFiles) => ({ ...prevFiles, [type]: file }))
-    }
+    const file = event.target.files?.[0] || null
+    setSelectedFiles((prevFiles) => ({ ...prevFiles, [type]: file }))
   }
 
   const handleAddFileToState = () => {
     if (selectedFiles.cv && selectedFiles.coverLetter) {
-      if (files.length > 1) {
-        dispatch(addCvCoverLetter([selectedFiles]))
-        setSelectedFiles(defaultValues)
-        if (cv.current) cv.current.value = ''
-        if (cover.current) cover.current.value = ''
-      }
       dispatch(addCvCoverLetter([selectedFiles]))
       setSelectedFiles(defaultValues)
       if (cv.current) cv.current.value = ''
       if (cover.current) cover.current.value = ''
     }
   }
+
+  const handleSubmit = async () => {
+    setShowAddButton(false)
+    let batchId = ''
+    setIsReevaluating(true)
+    for (const [index, file] of files.entries()) {
+      if (result[index]?.result) {
+        continue
+      }
+
+      const formData = new FormData()
+      formData.append('scoutJobId', scoutJobId)
+      if (file.cv) {
+        formData.append('cv', file.cv)
+      }
+      if (file.coverLetter) {
+        formData.append('coverLetter', file.coverLetter)
+      }
+      if (batchId) {
+        formData.append('batchId', batchId)
+      }
+
+      try {
+        const uploadResult = await handleUploadCvAndCoverLetter(
+          formData,
+        ).unwrap()
+        console.log(uploadResult)
+        if (uploadResult.status === true) {
+          dispatch(saveCvCoverLetterResult({ index, result: uploadResult }))
+          if (index === 0) {
+            batchId = uploadResult.data.scout.batchId
+          }
+        } else {
+          console.log('Error uploading CV and Cover Letter')
+        }
+      } catch (error) {
+        console.log('Upload Error:', error)
+      }
+
+      setAllFilesProcessed(
+        files.every((_, index) => result[index]?.result?.status === true),
+      )
+    }
+
+    if (!allFilesProcessed) {
+      setMoreFilesToProcess(true)
+    } else {
+      setMoreFilesToProcess(false)
+    }
+
+    setIsReevaluating(false)
+  }
+
+  const displayButton =
+    result.length === files.length &&
+    result.every((res) => res !== undefined && res !== null)
+      ? 'hidden'
+      : ''
+
+  const buttonTitle = isReevaluating ? (
+    <IsProcessing />
+  ) : moreFilesToProcess ? (
+    'Try evaluating again'
+  ) : files.length === 1 ? (
+    'Process Document'
+  ) : (
+    'Process Documents'
+  )
+
+  const getResultEvaluationScore = (index: number) =>
+    result.length > 0 ? result[index]?.result?.data?.evaluationScore : undefined
 
   return (
     <>
@@ -62,26 +142,34 @@ export const UploadCvAndCoverLetter = () => {
         description="Upload cv and cover letter in batch for collective AI assessment"
       />
       <div>
-        <div>
-          <p>Pick CV</p>
-          <input
-            type="file"
-            ref={cv}
-            onChange={(e) => handleFileChange(e, 'cv')}
+        {showAddButton && (
+          <>
+            <div>
+              <p>Pick CV</p>
+              <input
+                type="file"
+                ref={cv}
+                onChange={(e) => handleFileChange(e, 'cv')}
+              />
+            </div>
+            <div>
+              <p>Pick a Cover Letter</p>
+              <input
+                type="file"
+                ref={cover}
+                onChange={(e) => handleFileChange(e, 'coverLetter')}
+              />
+            </div>
+          </>
+        )}
+        {showAddButton && (
+          <RecruiterButton
+            buttonTitle={
+              files.length > 0 ? 'Add another profile' : 'Add profile'
+            }
+            onClick={handleAddFileToState}
           />
-        </div>
-        <div>
-          <p>Pick a Cover Letter</p>
-          <input
-            type="file"
-            ref={cover}
-            onChange={(e) => handleFileChange(e, 'coverLetter')}
-          />
-        </div>
-        <RecruiterButton
-          buttonTitle={files.length > 0 ? 'Add another profile' : 'Add profile'}
-          onClick={handleAddFileToState}
-        />
+        )}
 
         <div className={styles.uploader}>
           {files.length > 0 &&
@@ -91,11 +179,17 @@ export const UploadCvAndCoverLetter = () => {
                 key={index}
                 cv={file.cv}
                 coverLetter={file.coverLetter}
+                onDelete={() => dispatch(removeCvCoverLetter(index))}
+                result={getResultEvaluationScore(index)}
               />
             ))}
         </div>
         {files.length > 0 && (
-          <RecruiterButton buttonTitle="Process Documents" />
+          <RecruiterButton
+            buttonTitle={buttonTitle}
+            onClick={handleSubmit}
+            className={displayButton}
+          />
         )}
       </div>
     </>
