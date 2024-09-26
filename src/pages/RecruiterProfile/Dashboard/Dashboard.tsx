38144858197a6be1d8c87228787aadfc3fd5cc9a
Timmydee <dahunsitimmy@gmail.com>
2024-09-26 09:59:44 +0100

    refractor: dashboard component update

--- src/pages/RecruiterProfile/Dashboard/Dashboard.tsx
+++ src/pages/RecruiterProfile/Dashboard/Dashboard.tsx
@@ -1,20 +1,35 @@
 import React from 'react'
+import { Alert } from '@mui/material'
+
 import ApplicationTracker from './ApplicationTracker'
 import RecentApplications from './RecentApplications'
 import RecentCompilation from './RecentCompilation'
-import DashboardStats from './DashboardStats'
+import Loading from '../../../components/Loading/Loading'
+
+import { useFetchDashboardQuery } from '../../../redux/api/recruiter'
 
 const Dashboard = () => {
+  const {
+    data: dashboardData,
+    isLoading: dashboardLoading,
+    error: dashboardError,
+  } = useFetchDashboardQuery({})
+
+  if (dashboardLoading) return <Loading />
+
+  if (dashboardError) return <Alert severity="error">Error Fetching Data</Alert>
+
   return (
     <div className="w-[80%] mx-auto mt-10">
       <div>
-        <ApplicationTracker />
-        {/* <DashboardStats /> */}
+        <ApplicationTracker infoData={dashboardData.data.data} />
       </div>
-      <div className="mt-7">
-        <RecentApplications />
+      <div className="mt-10">
+        <RecentApplications
+          infoData={dashboardData.data.data.recentApplications}
+        />
       </div>
-      <div className="mt-7">
+      <div className="mt-10 h-[300px]">
         <RecentCompilation />
       </div>
     </div>
