c6db9297b8afd569a30c81647a8629a6792f09aa
Timmydee <dahunsitimmy@gmail.com>
2024-09-26 10:05:19 +0100

    feat: recent compilation update

--- src/pages/RecruiterProfile/Dashboard/RecentCompilation.tsx
+++ src/pages/RecruiterProfile/Dashboard/RecentCompilation.tsx
@@ -1,97 +1,112 @@
-import React from 'react'
-import { FiFilter } from 'react-icons/fi'
+import React, { useState, useEffect } from 'react'
+import FilterByJobs from '../../../components/Filter/FilterByJobs'
+import { useFetchDashboardQuery } from '../../../redux/api/recruiter'
+import Loading from '../../../components/Loading/Loading'
+import profilePics from '../../../assets/profilePics2.webp'
+import { Alert } from '@mui/material'
 
+interface CompilationTypes {
+  applicantName: string
+  role: string
+  cvSimilarityScore: string
+  prescreeningScore: string
+  personalizedAss: string
+  mbtiType: string
+}
 const RecentCompilation = () => {
-  const rows = [
-    {
-      name: 'Matthew Elijah',
-      role: 'Designer',
-      experience: '5 years',
-      metrics: {
-        cvMatch: '67%',
-        preScreening: '67%',
-        personalizedAss: '67%',
-        personality: 'INTJ',
-      },
-    },
-    {
-      name: 'Matthew Elijah',
-      role: 'Designer',
-      experience: '5 years',
-      metrics: {
-        cvMatch: '67%',
-        preScreening: '67%',
-        personalizedAss: '67%',
-        personality: 'INTJ',
-      },
-    },
-    {
-      name: 'Matthew Elijah',
-      role: 'Designer',
-      experience: '5 years',
-      metrics: {
-        cvMatch: '67%',
-        preScreening: '67%',
-        personalizedAss: '67%',
-        personality: 'INTJ',
-      },
-    },
-  ]
+  const [jobId, setJobId] = useState<string | null>(null)
+  const [infoData, setInfoData] = useState<CompilationTypes[]>([])
+
+  // Fetching data based on selected jobId
+  const {
+    data: dashboardData,
+    isLoading,
+    error,
+  } = useFetchDashboardQuery(jobId ? { jobId } : {})
+
+  // Update the state with the fetched data
+  useEffect(() => {
+    if (dashboardData?.data?.data?.recentCompilations) {
+      setInfoData(dashboardData.data.data.recentCompilations)
+    }
+  }, [dashboardData])
+
+  // Handle filter selection
+  const handleFilter = (selectedJobId: string) => {
+    setJobId(selectedJobId)
+  }
 
   return (
     <div className="py-4">
       <div className="flex justify-between items-center mb-4">
         <h4 className="text-lg font-bold">Recent Compilation</h4>
-        {/* Filter Button */}
-        <button className="flex items-center space-x-2 px-4 py-2 border border-gray-300 rounded-lg hover:bg-gray-100">
-          <FiFilter className="text-gray-600" />
-          <span className="text-gray-600">Filters</span>
-        </button>
+        <div>
+          <FilterByJobs onFilter={handleFilter} />
+        </div>
       </div>
-      <div className="space-y-8">
-        {rows.map((row, index) => (
-          <div
-            key={index}
-            className="flex justify-between p-4 bg-[#F8F8F8] rounded-2xl ">
-            <div className="flex items-center space-x-4">
-              <img
-                src="https://via.placeholder.com/50"
-                alt="avatar"
-                className="w-12 h-12 rounded-full"
-              />
-              <div>
-                <p className="text-lg font-semibold">{row.name}</p>
-                <p className="text-sm text-gray-500">{row.role}</p>
-                <p className="text-sm text-gray-500">{row.experience}</p>
-              </div>
-            </div>
 
-            {/* Right section with metrics */}
-            <div className="text-right space-y-2">
-              <div className="flex justify-between">
-                <span className="text-gray-600">CV Matching</span>
-                <span className="font-semibold">{row.metrics.cvMatch}</span>
+      {/* Compilation Data */}
+      {isLoading ? (
+        <div className="flex justify-center items-center h-32">
+          <Loading />
+        </div>
+      ) : error ? (
+        <Alert severity="error">Error Fetching Data</Alert>
+      ) : (
+        <div className="space-y-8">
+          {infoData.length > 0 ? (
+            infoData.map((row, index) => (
+              <div
+                key={index}
+                className="flex md:flex-row flex-col justify-between gap-8 p-4 bg-[#F8F8F8] rounded-2xl ">
+                <div className="flex items-center space-x-4">
+                  <img
+                    src={profilePics}
+                    alt="avatar"
+                    className="w-12 h-12 rounded-full"
+                  />
+                  <div>
+                    <p className="text-lg font-semibold">{row.applicantName}</p>
+                    <p className="text-sm font-medium text-[#0D0C22]">
+                      {row.role}
+                    </p>
+                  </div>
+                </div>
+
+                {/* Right section with metrics */}
+                <div className="text-right space-y-2">
+                  <div className="flex justify-between md:w-[200px] w-full">
+                    <span className="text-black">CV Matching</span>
+                    <span className="font-medium">
+                      score {row.cvSimilarityScore || 'nill'}
+                    </span>
+                  </div>
+                  <div className="flex justify-between md:w-[200px] w-full">
+                    <span className="text-black">Prescreening</span>
+                    <span className="font-medium">
+                      score {row.prescreeningScore || 'nill'}
+                    </span>
+                  </div>
+                  <div className="flex justify-between md:w-[200px] w-full">
+                    <span className="text-black">Personalised Ass</span>
+                    <span className="font-medium">
+                      score {row.personalizedAss || 'nill'}
+                    </span>
+                  </div>
+                  <div className="flex justify-between md:w-[200px] w-full">
+                    <span className="text-black">Personality Ass</span>
+                    <span className="font-medium">
+                      {row.mbtiType || 'nill'}
+                    </span>
+                  </div>
+                </div>
               </div>
-              <div className="flex justify-between">
-                <span className="text-gray-600">Prescreening</span>
-                <span className="font-semibold">
-                  {row.metrics.preScreening}
-                </span>
-              </div>
-              <div className="flex justify-between">
-                <span className="text-gray-600">Personalised Ass</span>
-                <span className="font-semibold">
-                  {row.metrics.personalizedAss}
-                </span>
-              </div>
-              <div className="flex justify-between">
-                <span className="text-gray-600">Personality Ass</span>
-                <span className="font-semibold">{row.metrics.personality}</span>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
+            ))
+          ) : (
+            <p>No compilations available for this Job</p>
+          )}
+        </div>
+      )}
     </div>
   )
 }
