c1ddd7a6b90dc8ef26413b3f0feb1692ecaba81e
mosidrum <mosiokanga@gmail.com>
2024-10-14 08:56:57 +0100

    fix: sign-in form validation

--- src/pages/Auth/AuthValidationSchema.ts
+++ src/pages/Auth/AuthValidationSchema.ts
@@ -22,3 +22,33 @@ export const loginSchema = Yup.object().shape({
   email: emailValidation(),
   password: passwordValidation(),
 })
+
+export const SignUpSchema = Yup.object().shape({
+  email: emailValidation(),
+  password: passwordValidation(),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref('password'), undefined], 'Passwords must match')
+    .required('Confirm Password is required'),
+
+  firstName: Yup.string()
+    .required('First Name is required')
+    .test('is-valid-name', 'First Name should only contain letters', (value) =>
+      /^[a-zA-Z]+$/.test(value || ''),
+    ),
+
+  lastName: Yup.string()
+    .required('Last Name is required')
+    .test('is-valid-name', 'Last Name should only contain letters', (value) =>
+      /^[a-zA-Z]+$/.test(value || ''),
+    ),
+
+  phone: Yup.string()
+    .required('Phone Number is required')
+    .test('valid-phone', 'Invalid phone number format', (value) =>
+      /^[0-9]{10,15}$/.test(value || ''),
+    ),
+
+  userType: Yup.string()
+    .oneOf(['talent', 'recruiter'], 'Invalid user type')
+    .required('User type is required'),
+})
