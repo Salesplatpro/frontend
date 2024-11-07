beca303742aa35ac827a29f9e0ac1abc0507bade
mosidrum <mosiokanga@gmail.com>
2024-11-07 02:29:57 +0100

    fix: and dropdown to logout and capitalize cv, cover letter names

--- src/pages/LoggedInUserBadge/LoggedInUserBadge.tsx
+++ src/pages/LoggedInUserBadge/LoggedInUserBadge.tsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import { IoIosArrowDown, IoMdNotificationsOutline } from 'react-icons/io'
-import { useSelector } from 'react-redux'
+import React, { useState } from 'react'
+import {
+  IoIosArrowDown,
+  IoIosArrowUp,
+  IoMdNotificationsOutline,
+} from 'react-icons/io'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 import employer from '../../assets/employer.png'
+import { logout } from '../../redux/features/authSlice/authSlice'
 import { RootState } from '../../redux/store/store'
 
 export const LoggedInUserBadge = () => {
+  const [visible, setVisible] = useState<boolean>(false)
   const user = useSelector((state: RootState) => state.auth.user)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
 
   return (
     <div className="employer">
@@ -20,7 +29,23 @@ export const LoggedInUserBadge = () => {
         </div>
         <img src={employer} alt="employer" />
       </div>
-      <IoIosArrowDown size={20} />
+      {visible ? (
+        <IoIosArrowUp size={20} onClick={() => setVisible(!visible)} />
+      ) : (
+        <IoIosArrowDown size={20} onClick={() => setVisible(!visible)} />
+      )}
+      {visible && (
+        <div className="absolute right-6 mt-32 w-64 text-center bg-white shadow-lg rounded-md overflow-hidden z-10">
+          <div
+            className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
+            onClick={() => {
+              dispatch(logout)
+              navigate('/')
+            }}>
+            Logout
+          </div>
+        </div>
+      )}
     </div>
   )
 }
