2654e3463dee0b2d1217eec8ed5fe090813728d9
Timmydee <dahunsitimmy@gmail.com>
2024-11-24 03:51:29 +0100

    refractor: chanaged based url

--- src/utils/baseConfig.ts
+++ src/utils/baseConfig.ts
@@ -1,1 +1,3 @@
-export const baseUrl = 'https://supportpro-backend.onrender.com/v1'
+export const baseUrl = 'http://167.172.8.238:8003/v1'
+
+// export const baseUrl = 'https://supportpro-backend.onrender.com/v1'
