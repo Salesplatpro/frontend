1d7e4193705cd8e3eb3266e892e29337240da6e2
mosidrum <mosiokanga@gmail.com>
2024-11-27 13:33:33 +0100

    fix: logged-in user

--- src/pages/MainLayout.tsx
+++ src/pages/MainLayout.tsx
@@ -1,15 +1,29 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { Navigate, Outlet } from 'react-router-dom'
 
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
+import { RootState } from '../redux/store/store'
 
-export const MainLayout = () => (
-  <>
-    <Navbar />
-    <main>
-      <Outlet />
-    </main>
-    <Footer />
-  </>
-)
+export const MainLayout = () => {
+  const user = useSelector((state: RootState) => state.auth)
+  const userRole = user.user?.userRole
+  const token = localStorage.getItem('token')
+
+  if (token && user.isLoggedIn) {
+    if (userRole === 'talent') return <Navigate to="/talentDashboard" replace />
+    if (userRole === 'recruiter')
+      return <Navigate to="/recruiterDashboard" replace />
+  }
+
+  return (
+    <>
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  )
+}
