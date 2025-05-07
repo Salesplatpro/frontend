7d92ee9441a4a357d76ee087e42282a70bda7576
Paul Boluwatife <paulboluwatife377@gmail.com>
2025-05-07 10:17:44 +0100

    fixed linting errors

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 import PricingHero from './Pricing/PricingHero'
 import PricePlan from './Pricing/PricePlan'
 import QuoteSection from './QuoteSection'
-import Footer from './Footer'
 
 const Pricing = () => {
   return (
--- src/components/Pricing/PricingCard.tsx
+++ src/components/Pricing/PricingCard.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import CheckBox from '../../assets/CheckIcon.svg'
 
 interface PricingDataProp {
