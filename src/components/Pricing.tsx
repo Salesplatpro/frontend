98ef885566c7b772c3c50377d4a99b55ea5b2934
Paul Boluwatife <paulboluwatife377@gmail.com>
2025-05-10 12:26:10 +0100

    price Notification implemented

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -3,12 +3,14 @@ import React from 'react'
 import PricingHero from './Pricing/PricingHero'
 import PricePlan from './Pricing/PricePlan'
 import QuoteSection from './QuoteSection'
+import PricingNotification from './Pricing/PricingNotification'
 
 const Pricing = () => {
   return (
     <div className="min-h-screen">
       <PricingHero />
       <PricePlan />
+      <PricingNotification />
       <QuoteSection />
     </div>
   )
