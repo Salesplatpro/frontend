2e3117d380d7fefb68229bf238048a00ed9fb08e
Paul Boluwatife <paulboluwatife377@gmail.com>
2025-05-14 16:52:43 +0100

    pricing update

---
+++ src/components/Pricing/PricingSwitch.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+
+interface PricingSwitchProp {
+  isFlipped: boolean
+  setIsFlipped: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const PricingSwitch: React.FC<PricingSwitchProp> = ({
+  isFlipped,
+  setIsFlipped,
+}) => {
+  const [isSelected, setIsSelected] = useState(true)
+
+  const toggle = () => {
+    setIsSelected(!isSelected)
+    setIsFlipped(!isFlipped)
+  }
+
+  return (
+    <div className="relative bg-[#F4EBFF] w-[320px] my-5 flex rounded-lg p-2 justify-between items-center overflow-hidden">
+      {/* Sliding background */}
+      <span
+        className={`absolute top-2 bottom-2 w-[48%] rounded-lg bg-white shadow-custom-1 transition-all duration-300 ease-in-out ${
+          isSelected ? 'left-2' : 'left-[50%]'
+        }`}
+      />
+
+      <button
+        onClick={toggle}
+        className={`relative z-10 w-1/2 px-2 py-2 text-[13px] rounded-lg lg:px-3 lg:py-3 lg:text-[18px] md:px-4 md:py-4 md:text-[16px] sm:px-3 sm:py-3 sm:text-[15px] font-inter font-medium text-center cursor-pointer transition-colors duration-300
+        ${isSelected ? 'text-[#4985DF]' : 'text-[#6941C6]'}`}>
+        Monthly billing
+      </button>
+
+      <button
+        onClick={toggle}
+        className={`relative z-10 w-1/2 px-2 py-2 text-[13px] rounded-lg lg:px-3 lg:py-3 lg:text-[18px] md:px-4 md:py-4 md:text-[16px] sm:px-3 sm:py-3 sm:text-[15px] font-inter font-medium text-center cursor-pointer transition-colors duration-300
+        ${!isSelected ? 'text-[#4985DF]' : 'text-[#6941C6]'}`}>
+        Annual billing
+      </button>
+    </div>
+  )
+}
+
+export default PricingSwitch
