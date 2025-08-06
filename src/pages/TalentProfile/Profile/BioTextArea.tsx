484a14938c3b0fbeb9e77957e78e9703142cc7d0
Paul Boluwatife <paulboluwatife377@gmail.com>
2025-08-06 04:44:04 +0100

    fixed bio character

---
+++ src/pages/TalentProfile/Profile/BioTextArea.tsx
@@ -0,0 +1,32 @@
+import { useFormikContext } from 'formik'
+import React from 'react'
+
+const BioTextArea = () => {
+  const { values, setFieldValue } = useFormikContext<any>()
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const value = e.target.value
+
+    if (value.length <= 1000) {
+      setFieldValue('bio', value)
+      console.log(value)
+    }
+  }
+
+  return (
+    <>
+      <textarea
+        id="bio"
+        name="bio"
+        value={values.bio}
+        onChange={handleChange}
+        placeholder="Tell us about yourself"
+        className="w-[100%] px-4 pb-16 rounded-lg border border-[#D0D5DD] h-[128px] mt-1 pt-4"
+      />
+
+      <p> {values.bio?.length || 0}/1000 </p>
+    </>
+  )
+}
+
+export default BioTextArea
