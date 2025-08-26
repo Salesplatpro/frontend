56c779ad46dd588181c0b686aaa77a4b5fbab86b
Paul Boluwatife <paulboluwatife377@gmail.com>
2025-08-26 10:45:09 +0100

    fixed the home icon

--- src/components/TalentProfile/SideBar/SideBarData.tsx
+++ src/components/TalentProfile/SideBar/SideBarData.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { BiMessageDetail } from 'react-icons/bi'
 import { BsChatDots } from 'react-icons/bs'
 import { IoBagOutline } from 'react-icons/io5'
-import { LuHome } from 'react-icons/lu'
+import { FaHome } from 'react-icons/fa'
 import { MdPersonOutline } from 'react-icons/md'
 import { MdOutlineAssessment } from 'react-icons/md'
 import { RiFlowChart } from 'react-icons/ri'
@@ -13,7 +13,7 @@ const COLOR = '#ffffff'
 export const sidebarData = [
   {
     name: 'Dashboard',
-    icon: <LuHome size={ICON_SIZE} color={COLOR} />,
+    icon: <FaHome size={ICON_SIZE} color={COLOR} />,
     link: '/talentDashboard/',
   },
   {
