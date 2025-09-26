d7cc736c3a6ff65b81b6fe61880114f7ef1fe693
Paul Boluwatife <paulboluwatife377@gmail.com>
2025-09-26 11:47:24 +0100

    fix the share button for talent individual job for talent and recruiter

--- src/pages/TalentProfile/Job/IndividualJob.tsx
+++ src/pages/TalentProfile/Job/IndividualJob.tsx
@@ -9,8 +9,9 @@ import { useIndividualJobQuery } from '../../../redux/api/talent'
 import { capitalizeFirstWord } from '../../../utils'
 import { capitalizeEachWord } from '../../../utils/CapitalizeWord'
 import { notify } from '../../../utils/toastNotifications'
-import ShareJobModal from './ShareJobModal'
-
+import Facebook from '../../../assets/Facebook icon.svg'
+import LinkedIn from '../../../assets/linkedin logo_icon.svg'
+import Twitter from '../../../assets/twitter_new_brand_icon.svg'
 import 'react-responsive-modal/styles.css'
 import { Modal } from 'react-responsive-modal'
 
@@ -93,6 +94,32 @@ const IndividualJob = () => {
     setIsModalOpen(true)
   }
 
+  const shareOptions = [
+    {
+      icon: Facebook,
+      text: 'Share',
+      action: 'share',
+      link: shareLinks.facebook,
+    },
+
+    {
+      icon: Twitter,
+      text: 'Tweet',
+      action: 'share',
+      link: shareLinks.twitter,
+    },
+    {
+      icon: LinkedIn,
+      text: 'Share',
+      action: 'share',
+      link: shareLinks.linkedin,
+    },
+  ]
+
+  const handleRedirectShare = (link: string) => {
+    window.open(link, '_blank', 'noopener,noreferrer')
+  }
+
   return (
     <div className="w-full">
       <div className="w-[96%] mx-auto mt-4">
@@ -100,11 +127,6 @@ const IndividualJob = () => {
           {jobProfile?.role && capitalizeEachWord(jobProfile?.role?.name)}
         </h2>
 
-        <button
-          onClick={() => setIsModalOpen(true)}
-          className="bg-blue-500 text-white px-4 py-2 rounded">
-          Open Modal
-        </button>
         <div className="flex space-x-5 items-center mt-3">
           <ShareOptions handleShare={handleShare} jobId={jobId!} />
         </div>
@@ -214,12 +236,27 @@ const IndividualJob = () => {
       )} */}
 
       <Modal open={isModalOpen} onClose={closeModal} center>
-        <h2>Simple centered modal</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-          pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
-          hendrerit risus, sed porttitor quam.
-        </p>
+        <div className="flex flex-col items-center text-center rounded-lg p-6 max-w-[400px] min-w-[280px] mx-auto">
+          <h2 className="text-[18px] sm:text-[20px] md:text-[22px] font-medium font-raleway">
+            Select your preferred social media to share job
+          </h2>
+
+          <div className="mt-4 w-full">
+            {shareOptions.map((option, index) => (
+              <button
+                key={index}
+                className="w-full border-[1px] py-3 my-2 rounded-lg font-raleway text-[14px] sm:text-[16px] md:text-[18px] bg-white border-[#E7E7E9] hover:bg-[#e8eaee] flex items-center justify-center"
+                onClick={() => handleRedirectShare(option.link)}>
+                <img
+                  src={option.icon}
+                  alt={option.text}
+                  className="mr-3 w-[28px] h-[28px] object-contain"
+                />
+                {option.text}
+              </button>
+            ))}
+          </div>
+        </div>
       </Modal>
     </div>
   )
