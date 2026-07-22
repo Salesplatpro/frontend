f23808342b4886316897e9891bb5e445d6d3b347
mosidrum <mosiokanga@gmail.com>
2026-07-22 07:48:03 +0100

    fix: keep sidebar nav item highlighted on nested routes

--- src/components/layout/sidebar/SideBar.tsx
+++ src/components/layout/sidebar/SideBar.tsx
@@ -14,6 +14,7 @@ interface sideBarProps {
     icon: ReactNode
     count?: number
     link?: string
+    end?: boolean
   }[]
   handleClick?: () => void
 }
@@ -52,6 +53,7 @@ export const SideBar: React.FC<sideBarProps> = ({
                 name={data.name}
                 count={data.count}
                 link={data.link}
+                end={data.end}
                 onClick={handleClick}
               />
             )
