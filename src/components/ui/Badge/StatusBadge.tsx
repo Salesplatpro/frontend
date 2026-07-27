2dca14ce456630204111048803db45b50c0a895e
mosidrum <mosiokanga@gmail.com>
2026-07-27 08:24:18 +0100

    feat: add optional dot indicator to StatusBadge

--- src/components/ui/Badge/StatusBadge.tsx
+++ src/components/ui/Badge/StatusBadge.tsx
@@ -6,17 +6,20 @@ type StatusBadgeProps = {
   status: string
   backgroundColor?: string
   color?: string
+  showDot?: boolean
 }
 
 export const StatusBadge = ({
   status,
   backgroundColor,
   color,
+  showDot,
 }: StatusBadgeProps) => {
   return (
     <div
       className={styles.statusBadge}
       style={{ background: backgroundColor, color }}>
+      {showDot && <span className={styles.dot} style={{ background: color }} />}
       {status}
     </div>
   )
